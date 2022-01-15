d858108c10a9dd690572be5dffcf8c19a4ae5533
Reuben Miller <reuben.miller@softwareag.com>
2022-01-15 22:52:17 +0100

    Publishing new release: v2.4.5

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Publish a package to the repository
+
+1. 
+
+```sh
+reprepro --basedir . includedeb buster ../debian/conf/*.deb
+```
+
+
+# References
+
+This repository was made possible by the following post:
+https://pmateusz.github.io/linux/2017/06/30/linux-secure-apt-repository.html
---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+REPO_BASE=$( cd "$SCRIPT_DIR/.." && pwd )
+
+echo "Removing any exising packages"
+rm -f "$REPO_BASE"/*.deb
+
+echo "Downloading latest release"
+tag_name=$( gh release view -R reubenmiller/go-c8y-cli --json tagName --jq ".tagName" )
+"$SCRIPT_DIR/download.sh"
+
+reprepro -V --basedir "$REPO_BASE" --component main includedeb go-c8y-cli "$REPO_BASE"/incoming/*.deb
+
+if ! git diff-index --quiet HEAD --; then
+    echo "Commit changes"
+    git add --all
+    git commit -m "Publishing new release: $tag_name"
+    git push
+fi
