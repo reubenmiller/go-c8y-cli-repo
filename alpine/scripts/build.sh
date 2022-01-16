0915a4f30aff68776eeafe877f411a16eb27f539
Reuben Miller <reuben.miller@softwareag.com>
2022-01-16 01:28:37 +0100

    alpine: fix repository file names

--- alpine/scripts/build.sh
+++ alpine/scripts/build.sh
@@ -27,6 +27,14 @@ index_packages () {
     mkdir -p "$CURRENT_BASE"
     cp "$INCOMING"/*"$filter"*.apk "$CURRENT_BASE/"
 
+    # Rename apk file to match: go-c8y-cli_2.4.5_linux_386.apk =>  go-c8y-cli-2.4.5.apk
+    # As the apk index expects it in this format
+    for apk_file in "$CURRENT_BASE/"*.apk
+    do
+        new_name=$( basename "$apk_file" | cut -d"_" -f1-2 | tr "_" "-" )
+        mv "$apk_file" "$CURRENT_BASE/${new_name}.apk"
+    done
+
     apk index -o "$CURRENT_BASE/APKINDEX.unsigned.tar.gz" "$CURRENT_BASE"/*.apk
     cp "$CURRENT_BASE/APKINDEX.unsigned.tar.gz" "$CURRENT_BASE/APKINDEX.tar.gz"
 
